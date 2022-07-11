86321e03df1967a1b85ea8ed50e00fe405469a2f
kanangit <kananovich@gmail.com>
2022-07-11 16:08:57 -0400

    working inject code

--- inject/inject/inject.ino
+++ inject/inject/inject.ino
@@ -12,7 +12,14 @@ const int stepsPerRevolution = 800;  // change this to fit the number of steps p
 // initialize the Stepper library on pins 9 and 8:
 StepperK myStepper(stepsPerRevolution, 9, 8, 10, 11);
 
+//define the trigger pin
+const int trigPin = 7;
+
+int steps_travel = stepsPerRevolution * 5;
+
 void setup() {
+  //set up the trig pin:
+  pinMode(trigPin, INPUT_PULLUP);
   // set the speed at xx rpm:
   myStepper.setSpeed(10);
   // initialize the serial port:
@@ -20,13 +27,15 @@ void setup() {
 }
 
 void loop() {
-  // step one revolution in one direction (forward):
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution * 2);
-  delay(500);
+// inject only if the trigger signal is received:
 
-  // step one revolution in the other direction:
-  //Serial.println("counterclockwise");
-  //myStepper.step(-stepsPerRevolution * 2);
- // delay(500);
+    if (digitalRead(trigPin) == LOW)
+    {
+      Serial.println("clockwise");
+      myStepper.step(steps_travel);
+      delay(500);
+    }
+
+
+
 }
