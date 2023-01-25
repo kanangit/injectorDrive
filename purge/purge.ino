9075ecdb0ac33e43d4b9c2210f8ffcd4ebb7bde7
kanangit <kananovich@gmail.com>
2023-01-25 12:07:55 -0500

    v. 019. inmplemented basic purge functionality

--- purge/purge.ino
+++ purge/purge.ino
@@ -11,36 +11,35 @@
 
 #include "StepperK.h"
 
+int totalStepsAllowed = 2540;
+int stepsTaken = 0;
+
 const int stepsPerRevolution = 800;  // change this to fit the number of steps per revolution
 // for your motor
 
 // initialize the StepperK library:
 StepperK myStepper(stepsPerRevolution, PUPIN, DIRPIN, LSCWPIN, LSCCWPIN, ACTIVATEMOTORPIN);
 
-//define the trigger pin
-const int trigPin = 7;
-
-int steps_travel =  stepsPerRevolution * 15;
-//int steps_travel =  25200;
+//int steps_travel =  stepsPerRevolution * 15;
+int steps_travel =  1;
 
 void setup() {
-  //set up the trig pin:
-  pinMode(trigPin, INPUT_PULLUP);
+
   // set the speed at xx rpm:
   myStepper.setSpeed(160);
   // initialize the serial port:
   //Serial.begin(9600);
+  //Prepare for motion, activate the motor:
+    digitalWrite(ACTIVATEMOTORPIN, HIGH);  
 }
 
 void loop() {
 // inject only if the trigger signal is received:
-
-    if (digitalRead(trigPin) == LOW)
-    {
-      
+  while ((stepsTaken < totalStepsAllowed) && (digitalRead(LSCWPIN) == LOW))
+  {
       myStepper.step(steps_travel);
-    }
-
+      stepsTaken++;
+  }
 
 
 }
